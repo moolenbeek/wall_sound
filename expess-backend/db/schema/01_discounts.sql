ed20234d07ff04066ce3aa1adce6eeb7b5e7756c
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-02-25 19:41:26 -0500

    update schema

--- expess-backend/db/schema/01_discounts.sql
+++ expess-backend/db/schema/01_discounts.sql
@@ -3,9 +3,9 @@ CREATE TABLE discount (
 id SERIAL PRIMARY KEY NOT NULL,
 discount_name VARCHAR(255) NOT NULL,
 discount_desc TEXT,
-discount_percent DECIMAL,
-active BOOLEAN,
-created_at TIMESTAMP,
+discount_percent DECIMAL NOT NULL,
+active BOOLEAN NOT NULL,
+created_at TIMESTAMP NOT NULL,
 modified_at TIMESTAMP,
 deleted_at TIMESTAMP
 );
