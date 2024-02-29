7131aae3763f2317ce3ac078b5282461725ba1df
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-02-28 21:11:16 -0500

    add react bootstrap and test with products

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import {useState, useEffect} from 'react';
 import './App.css';
-
 import axios from 'axios'
+
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
 
@@ -21,13 +24,29 @@ function App() {
 
   }, []);
 
-  const productList = products && products.map(product => <li key={product.id}> {product.product_name} </li>);
+  // <ListGroup.Item key={product.id}> {product.product_name} {product.flex_code}</ListGroup.Item>
+
+  const productList = products && products.map(product => 
+    <Card key={product.id} style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{product.product_name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{product.manufacturer}</Card.Subtitle>
+        <Card.Text>
+          Description goes here
+        </Card.Text>
+        <Card.Subtitle className="mb-2 text-muted">{product.price}</Card.Subtitle>
+        <Card.Link href="#">Add to cart</Card.Link>
+      </Card.Body>
+    </Card>
+  );
 
   return (
     <div className="App">
     
     <div>
+      <ListGroup as="ol" numbered>
       {productList}
+      </ListGroup>
     </div>
 
     </div>
