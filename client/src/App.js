4fd2850cba359a7ae65b0415a075e902161e5306
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-02-28 20:14:38 -0500

    add axios request and display products

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 import axios from 'axios'
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    axios({
+      method: 'get',
+      url: '/api/products'
+    })
+    .then(result => {
+      console.log(result.data)
+      setProducts(result.data);
+    })
+    .catch(err => console.log(`Error: err.message`))
+
+  }, [])
+
+  const productList = products && products.map(product => <li key={product.id}> {product.product_name} </li>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <ul>
+      {productList}
+    </ul>
+
     </div>
   );
 }
