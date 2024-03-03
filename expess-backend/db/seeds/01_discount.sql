ffac2a58fdcb3748825b85bf4d4c24b51980b556
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-03-02 22:56:47 -0500

    add seed data

--- expess-backend/db/seeds/01_discount.sql
+++ expess-backend/db/seeds/01_discount.sql
@@ -1,4 +1,5 @@
 INSERT INTO
 discount(discount_name, discount_desc, discount_percent, active, created_at)
 VALUES
-    ('twenty', 'save 20 percent', 20, true, CURRENT_TIMESTAMP);
+    ('twenty', 'save 20 percent', 20, true, CURRENT_TIMESTAMP),
+    ('fifty', 'save 50 percent', 50, true, CURRENT_TIMESTAMP);
